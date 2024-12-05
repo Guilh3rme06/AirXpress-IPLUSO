dff2c5ab683f3bc9d5296950b09ae4930ca477f3
Pedro Cunha <pedro.cunha@omnibees.com>
2024-12-05 10:48:44 +0000

    update voo_atualizar

--- util_voos.py
+++ util_voos.py
@@ -3,14 +3,32 @@ conexao = sqlite3.connect('airxpress-ipluso.db')
 cursor = conexao.cursor()
 
 def voo_consultar():
-    id = input('Número do voo -> ')
-    cursor.execute('SELECT * from voos where pk_voos = ?', (id))
+    id_voo = input('Número do voo -> ')
+    cursor.execute('SELECT * from voos where pk_voos = ?', (id_voo))
     conexao.commit()
 
 def voo_atualizar(titulo, autor, ano):
-    cursor.execute('UPDATE voos SET (titulo, autor, ano) VALUES (?,?,?)', (titulo, autor, ano))
+    id_voo = input('Número do voo -> ')
+    option = input(f'O que pretende mudar\n'
+                   '(1) origem\n'
+                   '(2) data\n'
+                   '(3) hora\n'
+                   '(4) capacidade\n'
+                   '(5) avião\n')
+    match option:
+        case '1':
+           # mudar origem
+        case '2':
+            # mudar data
+        case '3':
+            # mudar data
+        case '4':
+            # mudar data
+        case '5':
+            # mudar data
+    # cursor.execute('UPDATE voos SET (titulo, autor, ano) VALUES (?,?,?)', (titulo, autor, ano))
     conexao.commit()
 
 def voo_(eliminartitulo, autor, ano):
-    cursor.execute('INSERT INTO musicas (titulo, autor, ano) VALUES (?,?,?)', (titulo, autor, ano))
+    # cursor.execute('INSERT INTO musicas (titulo, autor, ano) VALUES (?,?,?)', (titulo, autor, ano))
     conexao.commit()
