f682ddf23c335766327310e225f6850be557803b
Guilherme Rodrigues <2006guiirodrigues@gmail.com>
2024-12-05 10:35:47 +0000

    update util_clientes.py

--- util_clientes.py
+++ util_clientes.py
@@ -4,21 +4,28 @@ conn = sqlite3.connect('AIRXPRESS.db')
 cursor = conn.cursor()
 
 cursor.execute('''
-    CREATE TABLE IF NOT EXISTS users(
+    CREATE TABLE IF NOT EXISTS clients(
     cliente INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
-    email TEXT
+    email TEXT UNIQUE
     )
 ''')
 conn.commit()
 
-def userRegist(name, email):
+def client_regist(name, email):
     for i in range(name):
         name = input("Write your name: ")
-        age = int(input("Write your age: "))
-        weight = float(input("Write your current weight: "))
-        height = float(input("Write your current height: "))
+        email = int(input("Write your email: "))
 
-    cursor.execute('INSERT INTO users (username, age) VALUES (?, ?)', (name, email))
+    cursor.execute('INSERT INTO clients (username, age) VALUES (?, ?)', (name, email))
     conn.commit()
     print(f"User {name} successfully created!")
+
+# Consultar uma lista de clientes registados
+def find_all_clients():
+    '''
+    Função para encontrar todos os clientes na tabela 'clients'.
+    :return: Uma lista com todos os clientes.
+    '''
+    cursor.execute('SELECT * FROM clients')
+    return cursor.fetchall()
